b0935d211786d82c43d2bfedd63cae26670be74b
Georgi Valchev <gvalchev@neterra.net>
2021-12-27 14:56:49 +0200

    Initial commit

---
+++ tests/AppBundle/Entity/DinosaurTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Test\AppBundle\Entity;
+
+use PHPUnit\Framework\TestCase;
+
+class DinosaurTest extends TestCase
+{
+    public function testThatYOurComputerWorks()
+    {
+        $this->assertTrue(false);
+    }
+}
