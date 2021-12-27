71fd2dad5238da9f5e332a163fb10d36510664ff
Georgi Valchev <gvalchev@neterra.net>
2021-12-27 15:13:11 +0200

    Added first tests

--- tests/AppBundle/Entity/DinosaurTest.php
+++ tests/AppBundle/Entity/DinosaurTest.php
@@ -2,12 +2,27 @@
 
 namespace Test\AppBundle\Entity;
 
+use AppBundle\Entity\Dinosaur;
 use PHPUnit\Framework\TestCase;
 
 class DinosaurTest extends TestCase
 {
-    public function testThatYOurComputerWorks()
+    public function testSettingLength()
     {
-        $this->assertTrue(false);
+        $dinosaur = new Dinosaur();
+
+        $this->assertSame(0, $dinosaur->getLength());
+
+        $dinosaur->setLength(9);
+        $this->assertSame(9, $dinosaur->getLength());
+    }
+
+    public function testDinosaurHasNotShrunk()
+    {
+        $dinosaur = new Dinosaur();
+
+        $dinosaur->setLength(15);
+
+        $this->assertGreaterThan(13, $dinosaur->getLength());
     }
 }
