a981268cd801ee612569163a3ad1ecddf1a67c6e
Georgi Valchev <gvalchev@neterra.net>
2022-02-02 20:43:54 +0200

    Added Exception when mixing dinosaurs in enclosure

--- src/AppBundle/Entity/Enclosure.php
+++ src/AppBundle/Entity/Enclosure.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Entity;
 
+use AppBundle\Exception\NotABuffetException;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
@@ -28,8 +29,21 @@ class Enclosure
         return $this->dinosaurs;
     }
 
+    /**
+     * @throws NotABuffetException
+     */
     public function addDinosaur(Dinosaur $dinosaur)
     {
+        if (! $this->canAddDinosaur($dinosaur)) {
+            throw new NotABuffetException();
+        }
+
         $this->dinosaurs[] = $dinosaur;
+    }
+
+    private function canAddDinosaur(Dinosaur $dinosaur): bool
+    {
+        return count($this->dinosaurs) === 0
+            || $this->dinosaurs->first()->isCarnivorous() === $dinosaur->isCarnivorous();
     }
 }
--- tests/AppBundle/Entity/EnclosureTest.php
+++ tests/AppBundle/Entity/EnclosureTest.php
@@ -5,6 +5,7 @@ namespace Tests\AppBundle\Entity;
 use AppBundle\Entity\Dinosaur;
 use AppBundle\Entity\Enclosure;
 use PHPUnit\Framework\TestCase;
+use AppBundle\Exception\NotABuffetException;
 
 class EnclosureTest extends TestCase
 {
@@ -23,5 +24,25 @@ class EnclosureTest extends TestCase
         $enclosure->addDinosaur(new Dinosaur());
 
         $this->assertCount(2, $enclosure->getDinosaurs());
+    }
+
+    public function testItDoesNotAllowCarnivorousDinosToMixWithHerbivores()
+    {
+        $enclosure = new Enclosure();
+        $enclosure->addDinosaur(new Dinosaur());
+
+        $this->expectException(NotABuffetException::class);
+
+        $enclosure->addDinosaur(new Dinosaur('Velociraptor', true));
+    }
+
+    /**
+     * @expectedException \AppBundle\Exception\NotABuffetException
+     */
+    public function testItDoesNotAllowToAddNonCarnivorousDinosaursToCarnivorousEnclosure()
+    {
+        $enclosure = new Enclosure();
+        $enclosure->addDinosaur(new Dinosaur('Velociraptor', true));
+        $enclosure->addDinosaur(new Dinosaur());
     }
 }
