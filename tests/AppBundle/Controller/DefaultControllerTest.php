73245458777c988639641b8b2e9bb510466bfe91
Georgi Valchev <gvalchev@neterra.net>
2022-02-13 22:37:36 +0200

    Added alarm button if enclosure don't have security

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -24,4 +24,21 @@ class DefaultControllerTest extends WebTestCase
         $table = $crawler->filter('.table-enclosures');
         $this->assertCount(3, $table->filter('tbody tr'));
     }
+
+    public function testThatThereIsAnAlarmButtonWithoutSecurity()
+    {
+        $fixtures = $this->loadFixtures([
+            LoadBasicParkData::class,
+            LoadSecurityData::class
+        ])->getReferenceRepository();
+
+        $client = $this->makeClient();
+
+        $crawler = $client->request('GET','/');
+
+        $enclosure = $fixtures->getReference('carnivorous-enclosure');
+        $selector = sprintf('#enclosure-%s .button-alarm', $enclosure->getId());
+
+        $this->assertGreaterThan(0, $crawler->filter($selector)->count());
+    }
 }
